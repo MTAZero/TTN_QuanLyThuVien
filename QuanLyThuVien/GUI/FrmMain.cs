adce140765dbafc19590f3051b9731e3f31e6162
taikhoana1 <noidovatoi11196@gmail.com>
2017-10-02 13:59:36 +0700

    formmain l1

--- QuanLyThuVien/GUI/FrmMain.cs
+++ QuanLyThuVien/GUI/FrmMain.cs
@@ -19,7 +19,28 @@ namespace QuanLyThuVien.GUI
         }
         #endregion
 
+        #region sự kiện
+        private void btnDauSach_Click(object sender, EventArgs e)
+        {
+            FrmQuanLyDauSach tg = new FrmQuanLyDauSach();
+            panelMain.Controls.Clear();
+            tg.TopLevel = false;
+            tg.Dock = DockStyle.Fill;
+            panelMain.Controls.Add(tg);
+            tg.Show();
+        }
 
-       
+        private void btnDocGia_Click(object sender, EventArgs e)
+        {
+            FrmQuanLyDocGia tg = new FrmQuanLyDocGia();
+            panelMain.Controls.Clear();
+            tg.TopLevel = false;
+            tg.Dock = DockStyle.Fill;
+            panelMain.Controls.Add(tg);
+            tg.Show();
+        }
+
+
+
     }
 }
