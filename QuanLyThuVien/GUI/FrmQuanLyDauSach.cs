8b36164862880bf94c2829a78debdc4315c8af8b
windatm <tuanphongmta0205@gmail.com>
2017-10-02 14:30:51 +0700

    FrmQuanLiDauSach1

--- QuanLyThuVien/GUI/FrmQuanLyDauSach.cs
+++ QuanLyThuVien/GUI/FrmQuanLyDauSach.cs
@@ -15,14 +15,51 @@ namespace QuanLyThuVien.GUI
     public partial class FrmQuanLyDauSach : Form
     {
 
+        private DauSachF DauSach_Service = new DauSachF();
+        private int index = 0, index1 = 0;
+
         #region constructor
         public FrmQuanLyDauSach()
         {
             InitializeComponent();
+            Data.Data.Reload();
         }
 
         #endregion
 
-    
+        #region LoadFrom
+
+        private void LoadDgvDauSach()
+        {
+            string key = txtTimKiem.Text;
+            int i = 0;
+            dgvDauSach.DataSource = DauSach_Service.DauSachS.ToList()
+                                    .Where(p => p.TEN.ToUpper().Contains(key.ToUpper()) || p.TACGIA.ToUpper().Contains(key.ToUpper()))
+                                    .Select(p => new
+                                    {
+                                        ID = p.ID,
+                                        STT = ++i,
+                                        TenSach = p.TEN,
+                                        TacGia = p.TACGIA
+                                    })
+                                    .ToList();
+
+            // cập nhật lại index
+            try
+            {
+                index = index1;
+                dgvDauSach.Rows[index].Cells["STT"].Selected = true;
+                dgvDauSach.Select();
+            }
+            catch { }
+        }
+        private void FrmQuanLyDauSach_Load(object sender, EventArgs e)
+        {
+            LoadDgvDauSach();
+            groupThongTin.Enabled = false;
+        }
+        #endregion
+
+
     }
 }
